24300e5ebf9803fb002782a47e9a746a5f9853d8
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-03-21 11:46:22 -0300

    Adicionadas páginas de descrição do encpsulamento de classes

--- Workshop WWDC.playground/Pages/- Playground Tutorial.xcplaygroundpage/Contents.swift
+++ Workshop WWDC.playground/Pages/- Playground Tutorial.xcplaygroundpage/Contents.swift
@@ -1,7 +1,21 @@
 //: [Previous](@previous)
-
-import Foundation
-
-var str = "Hello, playground"
-
+//: # Playground Tutorial
+/*: Então vamos finalmente mexer com a estrutura de um playground, e não só com o swift cru em si.
+ 
+ A graça do swift a bem da verdade, não é apenas a linguagem (apesar de eu achar ela beeeem legal)
+ Mas sim o que podemos fazer usando as funcionalidades e frameworks dos dispositivos da Apple, e do Playgrounds.
+ 
+ O *SwiftPlaygrouds* foi um projeto que a Apple criou para facilitar o aprendizado de código - eles tem o projeto *Everyone Can Code* com uma série de cursos e livros gratuitos para ensinar desenvolviemtno de software e apps em swift.
+ 
+ O curso do Everyone can code passa desde criaças do fundamental, até alunos universitários, professores, com vário níveis. E todos o slivros são disponibilizados gratuitamente on-line, e você pode encontrá-lo facilmente no iBooks ou iTunesU.
+ 
+ Mas nós que ja temos alguma noção de código, para que usamos os Playgrounds?
+ - Testar funcionalidades e frameworks novos
+ - Testar bibliotecas externas (Pods)
+ - Criar conteúdo de ensino
+ - (o que mais você quiser fazer **risos**)
+ 
+ */
+import PlaygroundSupport
+PlaygroundPage.current.liveView = GlowCircleViewController()
 //: [Next](@next)
