ceb484e626d429d72b14a02f2e3629291334ad5e
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-03-28 12:26:12 -0300

    Added new images

--- HuecoMundo.playground/Pages/Unusual Mission.xcplaygroundpage/Contents.swift
+++ HuecoMundo.playground/Pages/Unusual Mission.xcplaygroundpage/Contents.swift
@@ -2,7 +2,29 @@
 import UIKit
 import PlaygroundSupport
 //#-end-hidden-code
-
+//: # Hueco Mundo
+//: ## The little astronaut's mission on the Hollow World
+//: ### Giovani Nascimento Pereira - WWDC18
+/*:
+ Hello!
+ 
+ You are a member of the SEA - Space Exploration Academy.
+ Your duty is to follow our most important exploration missions remotely, and provide support to our astronaut explorers.
+ 
+ ![Logo of the Space Exploration Academy](SEA.png "Space Exploration Academy")
+ 
+ The next mission you will be following is called *Hueco Mundo*.
+ Our scanners found a strange planet, possibly inhabited, but with a very strange feature - the mass is too low, so probably we are talking about a hollow world.
+ 
+ Our little friend, the astronaut, was invited to a recognizing mission on this planet. Since it's completely new, we don't know what to expect.
+ 
+ You can follow the last images we have on him if you run the current code.
+ 
+ */
 PlaygroundPage.current.liveView = UnusualMissionStory()
-
+/*:
+ Something really wrong happened with the little astronaut!
+ 
+ And you are the only one who can save him!
+ */
 //: [Let's save the Astronaut!](@next)
